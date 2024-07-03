4e6b97c2ef85adc2729f5ceb1b7ab7f4908a79a1
Slowikxx <aniakslowik@student.agh.edu.pl>
2024-07-03 17:19:16 +0200

    - added obsidian-netherite pickaxe - added crafting for obsidian-netherite pickaxe

--- src/main/java/net/nutikas/opsidian/item/ModItems.java
+++ src/main/java/net/nutikas/opsidian/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.PickaxeItem;
 import net.nutikas.opsidian.OPsidian;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -12,10 +13,17 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 
+    // items
     public static final Item OBSIDIAN_INGOT = registerItem("obsidian_ingot", new Item(new FabricItemSettings()));
+    public static final Item NETHERITE_STICK = registerItem("netherite_stick", new Item(new FabricItemSettings()));
+
+    // tools
+    public static final Item OBSIDIAN_NETHERITE_PICKAXE = registerItem("obsidian_netherite_pickaxe", new PickaxeItem(ModToolMaterial.OBSIDIAN, 2, 2f, new FabricItemSettings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(OBSIDIAN_INGOT);
+        entries.add(NETHERITE_STICK);
+        entries.add(OBSIDIAN_NETHERITE_PICKAXE);
     }
 
     private static Item registerItem(String name, Item item) {
